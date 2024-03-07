0730847fa9df2661229e2b61a88ccaf3278483b1
Elias Solis <elias.e.solis@gmail.com>
2024-03-07 19:30:53 -0300

    first commit

---
+++ Fuua/src/componentes/principal/principal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./principal.css";
+const Principal = () => {
+  return (
+    <>
+      <div className="titulo">
+        <img
+          src="https://res.cloudinary.com/dtmziqubb/image/upload/v1709849712/Fuua_tittulo_tebxta.png"
+          alt=""
+        />{" "}
+      </div>
+      <div className="boton">
+        <button className="btn">Pedir</button>
+      </div>
+    </>
+  );
+};
+
+export { Principal };
