d79865a34f5d0ebbee5b8ad0621ef54d78530777
Elias Solis <elias.e.solis@gmail.com>
2024-03-11 13:29:35 -0300

    arreglo

--- src/componentes/categorias/index.jsx
+++ src/componentes/categorias/index.jsx
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom";
+import "./index.css";
 
 const Categorias = () => {
   return (
-    <div>
+    <div className="container-categorias">
       <h1>Categorias</h1>
       <ul>
-        <li>
-          <NavLink to="/wraps">Wraps</NavLink>
-        </li>
+        <div className="wraps">
+          <li className="title-wrap">
+            <NavLink to="/wraps">Wraps</NavLink>
+          </li>
+        </div>
         <li>
           <NavLink to="/ensaladas">Ensaladas</NavLink>
         </li>
